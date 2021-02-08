2bfd73efa2c8df98ae001f792589c14cd2f4a314
Matheus Penna <matheusnpenna@gmail.com>
2021-02-08 14:26:16 -0300

    :tada: And God said there is light

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3000
+
+// Create a new express application instance
+const app = express()
+
+// Call midlewares
+app.use(cors())
+app.use(helmet())
+app.use(bodyParser.json())
+
+app.post('/send-email', (req, res) => {
+    const { email, message } = req.body
+
+    if (!email || !/\S+@\S+\.\S+/.test(email)) {
+        res.status(400).send({
+            success: false,
+            errors: [
+                {
+                    message: 'Envie um e-mail válido',
+                },
+            ],
+        })
+        return
+    }
+
+    if (!message || !message.length) {
+        res.status(400).send({
+            success: false,
+            errors: [
+                {
+                    message: 'Envie uma mensagem válida',
+                },
+            ],
+        })
+        return
+    }
+
+    const nodemailer = require('nodemailer')
+
+    let transport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 587,
+        auth: {
+            user: 'naoresponda.qualybrasil@gmail.com',
+            pass: 'qualybrasilapp@10',
+        },
+    })
+    transport.sendMail(
+        {
+            from: 'naoresponda.qualybrasil@gmail.com', // Sender address
+            to: email, // List of recipients
+            subject: 'Coleta realizada - QualyBrasil Lavanderia', // Subject line
+            text: message, // Plain text body
+        },
+        function (err, info) {
+            if (err) {
+                res.status(400).send({
+                    success: false,
+                    errors: [
+                        {
+                            code: err.code,
+                            systemMessage: err.message,
+                            systemMessage2: err,
+                        },
+                    ],
+                })
+            } else {
+                res.status(200).send({
+                    success: true,
+                    message: `E-mail enviado!`,
+                })
+            }
+        }
+    )
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}!`)
+})
